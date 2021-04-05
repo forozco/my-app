8ccc7de4923a5a31f93320f7aee4f7e03976c3fe
Fernando Orozco <forozcop09@gmail.com>
2021-04-05 13:11:04 -0500

    Add libraries

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+// app.js
+// import { add } from './math.js';
+import { add } from './components/math.jsx'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Render s
         </p>
         <a
           className="App-link"
@@ -21,5 +25,7 @@ function App() {
     </div>
   );
 }
+
+console.log(add(16, 26)); // 42
 
 export default App;
